832cb3bc65a41b15f23b38668884486e87833d16
Denis Raygorodskiy <30652142+RokoDeveloper2@users.noreply.github.com>
2017-08-03 19:36:27 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,7 @@
 
 ini_set('display_errors',1);
 
-echo 1;
-exit();
+
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
