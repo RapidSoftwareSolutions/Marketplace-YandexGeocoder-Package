3b0be3054eac5859f3e734c2cbf720952dc5b059
Denis Raygorodskiy <30652142+RokoDeveloper2@users.noreply.github.com>
2017-08-03 19:37:28 +0300

    Update metadata.php

--- src/routes/metadata.php
+++ src/routes/metadata.php
@@ -2,6 +2,8 @@
 
 $app->get('/api/YandexGeocoder', function ($request, $response, $args) {
 
+       echo 1;
+exit();
     $newStream = new \GuzzleHttp\Psr7\LazyOpenStream(__DIR__ . '/../../src/metadata/metadata.json', 'r');
     $newResponse = $response->withHeader('Content-type', 'application/json')->withStatus(200)->withBody($newStream);
     return $newResponse;
